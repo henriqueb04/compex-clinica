31037daac5042cbde1ccf7adce3d02f5baf9f222
José Nelson <jose3202nelson@gmail.com>
2026-08-08 15:24:38 -0300

    CLI-01: Criação da entidade usuario e enum de sexo

---
+++ backend/src/main/java/com/compex/grupo5/enums/Sexo.java
@@ -0,0 +1,7 @@
+package com.compex.grupo5.enums;
+
+public enum Sexo {
+    Masculino,
+    Feminino,
+    Outro
+}
---
+++ backend/src/main/java/com/compex/grupo5/model/Usuario.java
@@ -0,0 +1,40 @@
+package com.compex.grupo5.model;
+
+import com.compex.grupo5.enums.Sexo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Usuario {
+
+    @Id
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Sexo sexo;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String telefone;
+}
