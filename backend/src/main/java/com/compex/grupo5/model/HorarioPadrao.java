2e092b1d91da30ab35363817ed4376d4530b4c37
josef_lp <jose.lp.felipe@gmail.com>
2026-08-08 23:42:07 -0300

    criação do model HorarioPadrao

---
+++ backend/src/main/java/com/compex/grupo5/model/HorarioPadrao.java
@@ -0,0 +1,34 @@
+package com.compex.grupo5.model;
+
+import io.hypersistence.utils.hibernate.type.range.PostgreSQLRangeType;
+import io.hypersistence.utils.hibernate.type.range.Range;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import java.time.DayOfWeek;
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "horarios_padrao")
+public class HorarioPadrao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "dia_semana", nullable = false, length = 15)
+    private DayOfWeek diaSemana;
+
+    @Type(PostgreSQLRangeType.class)
+    @Column(name = "intervalo_atendimento", columnDefinition = "tstzrange", nullable = false)
+    private Range<ZonedDateTime> intervaloAtendimento;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_profissional", nullable = false)
+    private Profissional profissional;
+}
