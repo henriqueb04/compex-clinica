b41c332364916d68b3b5d303b1f73b3daf90645b
José Henrique Brito Oliveira <hriquebrito37@outlook.com>
2026-08-10 01:54:08 -0300

    feat(service): HorarioDisponivelDto implementado

---
+++ backend/src/main/java/com/compex/grupo5/dto/HorarioDisponivelDto.java
@@ -0,0 +1,46 @@
+package com.compex.grupo5.dto;
+
+import com.compex.grupo5.dao.ProfissionalRepository;
+import com.compex.grupo5.model.HorarioDisponivel;
+import io.hypersistence.utils.hibernate.type.range.Range;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+import java.time.DayOfWeek;
+import java.time.ZonedDateTime;
+
+/**
+ * DTO for {@link com.compex.grupo5.model.HorarioDisponivel}
+ */
+public record HorarioDisponivelDto(
+        Long id,
+        @NotNull Integer ano,
+        @NotNull DayOfWeek diaSemana,
+        @NotNull Integer numeroSemana,
+        @NotNull ZonedDateTime comeco,
+        @NotNull ZonedDateTime fim,
+        @NotBlank String profissional_cpf
+) implements Serializable {
+    public HorarioDisponivel toEntity(ProfissionalRepository profissionalRepository) {
+        HorarioDisponivel horarioDisponivel = new HorarioDisponivel();
+        horarioDisponivel.setAno(this.ano);
+        horarioDisponivel.setDiaSemana(this.diaSemana);
+        horarioDisponivel.setNumeroSemana(this.numeroSemana);
+        horarioDisponivel.setIntervaloAtendimento(Range.closed(this.comeco, this.fim));
+        horarioDisponivel.setProfissional(profissionalRepository.findByCpfIs(this.profissional_cpf));
+        return horarioDisponivel;
+    }
+
+    public static HorarioDisponivelDto fromEntity(HorarioDisponivel horario) {
+        return new HorarioDisponivelDto(
+                horario.getId(),
+                horario.getAno(),
+                horario.getDiaSemana(),
+                horario.getNumeroSemana(),
+                horario.getIntervaloAtendimento().lower(),
+                horario.getIntervaloAtendimento().upper(),
+                horario.getProfissional().getCpf()
+        );
+    }
+}
